2030ef515a95de04fe5e349ca731635fe0ddef46
Nitishjhaa <nj9876543210p@gmail.com>
2025-03-05 23:33:37 +0530

    added useContext

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ApiProvider } from './context/ApiContext.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <ApiProvider>
     <App />
-  </StrictMode>,
+  </ApiProvider>,
 )
